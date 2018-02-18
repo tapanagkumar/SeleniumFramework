dd6f1fb3252d3dd6a8606faf0a72f22e750c6b59
Gowri <Gowri@LAPTOP-GOWRISAIAARUSH.mshome.net>
2018-02-18 12:19:35 +1100

    Added functions for Excel Functionality

--- End2End/src/main/java/com/gowri/End2End/Base.java
+++ End2End/src/main/java/com/gowri/End2End/Base.java
@@ -1,16 +1,17 @@
 package com.gowri.End2End;
 
-
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-
 import org.openqa.selenium.By;
 import org.apache.commons.io.FileUtils;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -19,8 +20,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-
-
 /**
  * @author Gowri
  *
@@ -28,19 +27,26 @@ import org.openqa.selenium.support.ui.Select;
 public class Base {
 	public static WebDriver driver;
 	public Properties prop;
-	public static Select mySelect; 
+	public static Select mySelect;
+	public static Properties Param;
+	public static XSSFWorkbook wb;
+	public static XSSFSheet ws;
+	public static XSSFSheet ws1;
+	public static FileInputStream fin;
+	public static FileOutputStream fout;
 
 	public WebDriver initializeDriver() throws IOException {
 
 		prop = new Properties();
 		FileInputStream fis = new FileInputStream(
-				System.getProperty("user.dir")+"\\src\\main\\java\\com\\gowri\\Resources\\data.properties");
+				System.getProperty("user.dir") + "\\src\\main\\java\\com\\gowri\\Resources\\data.properties");
 		prop.load(fis);
 		String browserName = prop.getProperty("Browser");
 		System.out.println(browserName);
 
 		if (browserName.equals("Chrome")) {
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\main\\java\\com\\gowri\\Resources\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\src\\main\\java\\com\\gowri\\Resources\\chromedriver.exe");
 			driver = new ChromeDriver();
 			// execute in chrome driver
 
@@ -51,17 +57,29 @@ public class Base {
 			// IE code
 		}
 
-		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		return driver;
 
 	}
-	
-	public void getScreenshot(String result) throws IOException
-	{
-		File src=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		FileUtils.copyFile(src, new File("C://test//"+result+"screenshot.png"));
-		
+
+	// Get Screenshots on Failure
+	public void getScreenshot(String result) throws IOException {
+		File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C://test//" + result + "screenshot.png"));
+
+	}
+
+	// Read from Excel
+	public void ReadFromExcel() throws Exception {
+		// Read the login details from excel sheet stored in resources folder
+
+		// file for reading the user name and password
+		fin = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\main\\java\\com\\gowri\\Resources\\Contacts.xlsx");
+		wb = new XSSFWorkbook(fin);
+		ws = wb.getSheet("Contacts");
+		// ws1=wb.getSheet("Registration");
+
 	}
 
 }
